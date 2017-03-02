f3f4eda6a35ef92e3ef595811e013345c4b48c3f
doctorPacMan <artempoo@gmail.com>
2017-03-02 20:33:18 +0700

    save

--- hell/demon.php
+++ hell/demon.php
@@ -1,4 +1,20 @@
 <?php
+/*
+$data = "GET / HTTP/1.1";
+$data.="\r\nHost: cherio.io:889";
+$data.="\r\nConnection: Upgrade";
+$data.="\r\nPragma: no-cache";
+$data.="\r\nCache-Control: no-cache";
+$data.="\r\nUpgrade: websocket";
+$data.="\r\nOrigin: http://cherio.io";
+$data.="\r\nSec-WebSocket-Version: 13";
+$data.="\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
+$data.="\r\nAccept-Encoding: gzip, deflate, sdch";
+$data.="\r\nAccept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4";
+$data.="\r\nCookie: _ga=GA1.2.909379193.1471585713";
+$data.="\r\nSec-WebSocket-Key: VtatAXIadIv94WX/Tt3nuA==";
+$data.="\r\nSec-WebSocket-Extensions: permessage-deflate; client_max_window_bits\r\n\r\n";
+*/
 function daemonTick($pid, $cnt) {
 	echo("daemonTick: ".$cnt);
 
@@ -15,33 +31,59 @@ function daemonDone($pid, $cnt) {
 	echo("daemonDone: ".$cnt."\r\n");
 };
 function trace($str) {echo($str);return $str;};
+function ws_getheaders($data) {
 
+	// check for valid http-header:
+    //if(!preg_match('/\AGET (\S+) HTTP\/1.1\z/', $lines[0], $matches))
+
+	$lines = preg_split("/\r\n/",rtrim($data));
+	$lines[0] = "info: ".$lines[0];
+	
+	$headers = array();
+	foreach($lines as $line)
+		if(preg_match('/\A(\S+): (.*)\z/', rtrim($line), $matches)) $headers[$matches[1]] = $matches[2];
+
+	return $headers;
+};
 function ws_getbuffer($ws_resource) {
 	
+	$socket = $ws_resource;
 	$client = socket_accept($ws_resource);
 	echo "\r\n\tsocket_accept ".($client===FALSE ? "failure" : "success")." ".$client;
-	//return $ws_resource." client:".$client;
+	//return $socket." client:".$client;
 	
 	$result = "\r\n\t";
-	$buffer = 'BUFFER';
-	$bytes = socket_recv($ws_resource, $buffer, 4096, MSG_WAITALL); // получаем информацию от сокета
+	$buffer = NULL;
+	$bytes = @socket_recv($client, $buffer, 4096, MSG_WAITALL); // получаем информацию от сокета
 	
-	if ($bytes===FALSE) $result.= "socket_recv failure: ".socket_strerror(socket_last_error());
-	else if($bytes===0) $result.= "socket_recv failure: 0 bytes recived";
+	if ($bytes===FALSE) $result.= "socket_recive failure: ".socket_strerror(socket_last_error());
+	else if($bytes===0) $result.= "socket_recive failure: 0 bytes recived";
 	else {
     	$result.= "socket_recv success: ".$bytes." bytes recived";
-	}
+    	//echo("BUFFER >\r\n".$buffer."\r\n< BUFFER\r\n");
+	};
+
+	if(!$bytes) socket_close($socket);
+	else {
+    	$headers = ws_getheaders($buffer);
+    	$response = ws_handshake($headers['Sec-WebSocket-Key']);
+		$rs = socket_write($client, $response, strlen($response));
+		$result.= "\r\n\tsocket_write handshake ".$rs;
+		echo("\r\n>>>\r\n".$response."<<<<");
+	};
 	echo $result;
-	//socket_close($socket);
+	return $buffer;
 };
-function ws_handshake($ws_resource) {
+function ws_handshake($key) {
+	$hash = $key.'258EAFA5-E914-47DA-95CA-C5AB0DC85B11'; 
+	$hash = base64_encode(sha1($hash,true));
 	$upgrade = "HTTP/1.1 101 Switching Protocols\r\n";
 	$upgrade.= "Upgrade: websocket\r\n";
 	$upgrade.= "Connection: Upgrade\r\n";
-	$upgrade.= "Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=\r\n";
+	$upgrade.= "Sec-WebSocket-Accept: ".$hash."\r\n";
+	$upgrade.= "Sec-WebSocket-Version: 13\r\n";
 	$upgrade.= "\r\n";
-
-	return $ws_resource;
+	return $upgrade;
 };
 function getWebsocket() {
 
